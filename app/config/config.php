bced445dca15d12fc628f00d6077b0d1e3f2634a
javialex <lexloxa@gmail.com>
2014-11-05 19:37:05 -0400

    Ultima actualizacion del dia

--- app/config/config.php
+++ app/config/config.php
@@ -3,21 +3,9 @@
 return new \Phalcon\Config(array(
     'database' => array(
         'adapter'     => 'Postgresql',
-<<<<<<< HEAD
         'host'        => '192.168.10.158',//localhost
         'username'    => 'postgres',////oasisuser
         'password'    => 'miteleferico123',//oasispass
-=======
-<<<<<<< HEAD
-        'host'        => '192.168.10.158',
-        'username'    => 'postgres',
-        'password'    => 'miteleferico123',
-=======
-        'host'        => 'localhost',//192.168.10.158
-        'username'    => 'user_rrhh',//postgres
-        'password'    => 'pass_rrhh',//miteleferico123
->>>>>>> 9f964e2a2971495c96965c1ce02162c78f48af0e
->>>>>>> 411a08262e4c84e24d18e520c992a8caf69508b6
         'dbname'      => 'bd_rrhh',
     ),
     'application' => array(
@@ -27,6 +15,7 @@ return new \Phalcon\Config(array(
         'pluginsDir'     => __DIR__ . '/../../app/plugins/',
         'libraryDir'     => __DIR__ . '/../../app/library/',
         'cacheDir'       => __DIR__ . '/../../app/cache/',
+        'baseUri'        => '   ',
         // Cargar librería fpdf
         'fpdf'        => __DIR__ . '/../../app/libs/fpdf/',
         'baseUri'        => '',
--- public/js/relaborales/oasis.relaborales.view.splitter.js
+++ public/js/relaborales/oasis.relaborales.view.splitter.js
@@ -5,7 +5,11 @@
  *   Usuario Creador: Lic. Javier Loza
  *   Fecha Creación:  05-11-2014
  */
-function cargarHistorialRelaboral(idPersona){
+/**
+ * Función para cargar el listado de años en los cuales la persona (Identificada por el parámetro idPersona) ha trabajado o trabaja en la empresa.
+ * @param idPersona Identificador de la persona de la cual se desea obtener la información respectiva.
+ */
+function cargarGestionesHistorialRelaboral(idPersona){
     var gestiones = [];
     $('#HistorialSplitter').jqxSplitter('expand');
     $.ajax({
@@ -52,6 +56,31 @@ function cargarHistorialRelaboral(idPersona){
             else checkedItems += this.label;
         });
         $("#CheckedItems").text(checkedItems);
+    });
+}
+/**
+ * Función para cargar el historial de relación laboral para una persona en particular considerando un gestión específica (Si el valor para el parámetro gestion es mayor a cero)
+ * @param idPersona Identificador de la persona de la cual se requiere obtener el historial.
+ * @param gestion Gestión específica de la cual se desea obtener la información. En caso de que su valor sea cero el historial mostrará el total del historial.
+ */
+function cargarHistorialRelacionLaboral(idPersona,gestion){
+    var gestiones = [];
+    //$('#HistorialTimeLine').html("");
+    $.ajax({
+        url:'/relaborales/listhistorial',
+        type:'POST',
+        datatype: 'json',
+        async:false,
+        cache:false,
+        data:{id:idPersona},
+        success: function(data) {
+            /*var res = jQuery.parseJSON(data);
+            if(res.length>0){
+                $.each( res, function( key, val ) {
+                    //gestiones.push(val.gestion);
+                });
+            }*/
+        }
     });
 
 }
