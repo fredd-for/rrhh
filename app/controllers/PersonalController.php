6507e61ac0a0aa33fd69a6e8006a9f28d7c76cb0
SadnessLord <sadness.lord@gmail.com>
2014-11-06 10:17:33 -0400

    subida 2014-11-06 10:17 taddeo

--- app/controllers/PersonalController.php
+++ app/controllers/PersonalController.php
@@ -110,6 +110,8 @@ class PersonalController extends ControllerBase{
                 'lugar_nac' => $v->lugar_nac,
                 'genero' => $v->genero,
                 'e_civil' => $v->e_civil,
+                'tipo_doc' => $v->tipo_doc,
+                'expd'=> $v->expd,
             );
         }
         echo json_encode($customers);
@@ -404,29 +406,92 @@ class PersonalController extends ControllerBase{
 	echo json_encode($msm);
         //echo $msm;
     }
-    public function imprimirAction($n_rows, $rows, $columns){
-        $rows = base64_decode(str_pad(strtr($rows, '-_', '+/'), strlen($rows) % 4, '=', STR_PAD_RIGHT)); 
+    public function imprimirAction($n_rows, $rows, $columns, $filtros){
+        //$rows = base64_decode(str_pad(strtr($rows, '-_', '+/'), strlen($rows) % 4, '=', STR_PAD_RIGHT)); 
         $columns = base64_decode(str_pad(strtr($columns, '-_', '+/'), strlen($columns) % 4, '=', STR_PAD_RIGHT));
+        $filtros = base64_decode(str_pad(strtr($filtros, '-_', '+/'), strlen($columns) % 4, '=', STR_PAD_RIGHT));
         //echo $rows." - ".$columns;
         $pdf = new FPDF();
         //$rows = (string)$rows;
-        $rows = json_decode($rows,true);
+        
+        //echo $filtros;
+        //$rows = json_decode($rows,true);
         $columns = json_decode($columns,true);
+        $filtros = json_decode($filtros,true);
         $pdf->AddPage('L','Letter');
         //$pdf->SetFont('Arial','B',16);
-        $keys = array_keys($rows[0]);
-        $n_col = count($keys);//echo$keys[1];
+        $sub_keys = array_keys($columns);//echo $sub_keys[0];
+        //$keys = array_keys($rows[0]);
+        $n_col = count($columns);//echo$keys[1];
+        //echo $n_col;
+        $title = utf8_decode('Reporte de Personal "Mi teleférico".');
+        $pdf->SetFont('Arial','B',12);
+        $w = $pdf->GetStringWidth($title)+6;
+        $pdf->SetX((260-$w)/2);
+        $pdf->SetDrawColor(0,80,80);
         $pdf->SetFillColor(0,153,153);
         $pdf->SetTextColor(255);
-        $pdf->SetDrawColor(0,120,120);
-        $pdf->SetLineWidth(.3);
-        $pdf->SetFont('Arial','B',6);
-        $pdf->Cell(10,7,'Nro.',1,0,'C',true);
-        for ($j=0;$j<$n_col-1;$j++){
-            if ($keys[$j] != 'dataindex'){
-                if ($columns[$keys[$j]]['hidden'] == FALSE){
-                    $pdf->Cell(24,7,$columns[$keys[$j]]['text'],1,0,'C',true);
+        // Ancho del borde (1 mm)
+        $pdf->SetLineWidth(1);
+        // Título
+        $pdf->Cell($w+15,9,$title,1,1,'C',true);
+        $pdf->Ln();
+        $pdf->SetFont('Arial','',10);
+        // Color de fondo
+        $pdf->SetFillColor(255,255,255);
+        $pdf->SetTextColor(0);
+        // Título
+        $pdf->Cell(0,6,"Filtrado por:",0,1,'L',true);
+        $where = '';
+        for($k=0;$k<count($filtros);$k++){
+            for ($j=0;$j<$n_col;$j++){
+                if ($sub_keys[$j] == $filtros[$k]['columna']){
+                    $col_fil = $columns[$sub_keys[$j]]['text'];//echo $col_fil;
                 }
+            }
+            $cond_fil = ' '.$col_fil;
+            if (strlen($where)>0){
+                $where .= ' AND ';
+            }
+            if ($filtros[$k]['tipo'] == 'datefilter'){
+                $filtros[$k]['valor'] = date("Y-m-d",strtotime($filtros[$k]['valor']));
+                //echo $filtros[$k]['valor'];
+            }
+            switch ($filtros[$k]['condicion']){
+            /*case 'EMPTY':
+                $cond_fil .= utf8_encode(" que sea vacía ");
+                $where .= $filtros[$k]['columna'].
+                break;
+            case 'NOT_EMPTY':
+                $cond_fil .= utf8_encode(" que no sea vacía ");
+                break;*/
+            case 'CONTAINS':
+                $cond_fil .= utf8_encode(" que contenga el valor:  ".$filtros[$k]['valor']);
+                $where .= $filtros[$k]['columna'].' ILIKE "%'.$filtros[$k]['valor'].'%"';
+                break;
+            case 'GREATER_THAN_OR_EQUAL':
+                $cond_fil .= utf8_encode(" que sea mayor o igual que:  ".$filtros[$k]['valor']);
+                $where .= $filtros[$k]['columna'].' >= "'.$filtros[$k]['valor'].'"';
+                break;
+            case 'LESS_THAN_OR_EQUAL':
+                $cond_fil .= utf8_encode(" que sea menor o igual que:  ".$filtros[$k]['valor']);
+                $where .= $filtros[$k]['columna'].' <= "'.$filtros[$k]['valor'].'"';
+                break;
+            }//echo $cond_fil;
+            $pdf->Cell(0,6,  utf8_decode($cond_fil),0,1,'L',true);
+        }
+        //echo $where;
+        // Salto de línea
+        $pdf->Ln(4);
+        $pdf->SetFillColor(0,153,153);
+        $pdf->SetTextColor(255);
+        $pdf->SetDrawColor(0,80,80);
+        $pdf->SetLineWidth(.3);
+        $pdf->SetFont('Arial','B',8);
+        $pdf->Cell(10,7,'Nro.',1,0,'C',true);
+        for ($j=0;$j<$n_col;$j++){
+            if ($columns[$sub_keys[$j]]['hidden'] == FALSE){
+                $pdf->Cell(35,7,$columns[$sub_keys[$j]]['text'],1,0,'C',true);
             }
         }
         $pdf->Ln();
@@ -435,18 +500,35 @@ class PersonalController extends ControllerBase{
         $pdf->SetFont('');
         $fill = false;
         $ancho = 0;
+        $resul = personas::find(array($where,'order' => 'id ASC'));
+        //$this->view->disable();
+        foreach ($resul as $v) {
+            $personal[] = array(
+                'id' => $v->id,
+                'p_nombre' => $v->p_nombre,
+                's_nombre' => $v->s_nombre,
+                'p_apellido' => $v->p_apellido,
+                's_apellido' => $v->s_apellido,
+                'ci' => $v->ci,
+                'fecha_nac' => date("d-m-Y",strtotime($v->fecha_nac)),
+                'lugar_nac' => $v->lugar_nac,
+                'genero' => $v->genero,
+                'e_civil' => $v->e_civil,
+                'tipo_doc' => $v->tipo_doc,
+                'expd'=> $v->expd,
+            );
+        } //echo $personal[0]['id'];
         for ($i=0;$i<$n_rows;$i++){
             $pdf->Cell(10,6,$i,'LR',0,'L',$fill);
             $ancho = 10;
-            for ($j=0;$j<$n_col-1;$j++){
-                if ($keys[$j] != 'dataindex'){
-                    if ($columns[$keys[$j]]['hidden'] == FALSE){
-                        $pdf->Cell(24,6,  utf8_decode($rows[$i][$keys[$j]]),'LR',0,'L',$fill);
-                        $ancho = $ancho + 24;
+            for ($j=0;$j<$n_col;$j++){
+                if ($columns[$sub_keys[$j]]['hidden'] == FALSE){
+                        $pdf->Cell(35,6,  utf8_decode($personal[$i][$sub_keys[$j]]),'LR',0,'L',$fill);
+                        $ancho = $ancho + 35;
                     }
                 }
             //$pdf->Cell(40,10, ($keys[$j]),0,1);
-            }
+            
             $fill = !$fill;
             $pdf->Ln();
             //$pdf->Cell(40,10, ($rows[$i]['id']),0,1);
