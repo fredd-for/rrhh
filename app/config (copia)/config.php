ed9eb915fd906b39ab1cd8e1f5b9f6a4074a1a9d
fredd_for <fredd_for@hotmail.com>
2014-11-11 21:54:08 -0400

    copia config

---
+++ app/config (copia)/config.php
@@ -0,0 +1,23 @@
+<?php
+
+return new \Phalcon\Config(array(
+    'database' => array(
+        'adapter'     => 'Postgresql',
+        'host'        => 'localhost',
+        'username'    => 'postgres',
+        'password'    => '8209125',
+        'dbname'      => 'bd_rrhh',
+    ),
+    'application' => array(
+        'controllersDir' => __DIR__ . '/../../app/controllers/',
+        'modelsDir'      => __DIR__ . '/../../app/models/',
+        'viewsDir'       => __DIR__ . '/../../app/views/',
+        'pluginsDir'     => __DIR__ . '/../../app/plugins/',
+        'libraryDir'     => __DIR__ . '/../../app/library/',
+        'cacheDir'       => __DIR__ . '/../../app/cache/',
+        'baseUri'        => '   ',
+        // Cargar librería fpdf
+        'fpdf'        => __DIR__ . '/../../app/libs/fpdf/',
+        'baseUri'        => '',
+    )
+));
