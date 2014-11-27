322c22083b10ea17c6330ef9b083a238d2bd57e2
javialex <lexloxa@gmail.com>
2014-11-27 11:55:32 -0400

    Ultima actualizacion de Movilidad de Personal

--- app/models/Departamentos.php
+++ app/models/Departamentos.php
@@ -1,13 +1,72 @@
 <?php
-/*
-*   Oasis - Sistema de Gestión para Recursos Humanos
-*   Empresa Estatal de Transporte por Cable "Mi Teleférico"
-*   Versión:  1.0.0
-*   Usuario Creador: Lic. Javier Loza
-*   Fecha Creación:  24-11-2014
-*/
-use Phalcon\Mvc\Model\Resultset\Simple as Resultset;
 
-class departamentos extends \Phalcon\Mvc\Model {
+class Departamentos extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var integer
+     */
+    public $pais_id;
+
+    /**
+     *
+     * @var string
+     */
+    public $departamento;
+
+    /**
+     *
+     * @var string
+     */
+    public $observacion;
+
+    /**
+     *
+     * @var integer
+     */
+    public $estado;
+
+    /**
+     *
+     * @var integer
+     */
+    public $baja_logica;
+
+    /**
+     *
+     * @var integer
+     */
+    public $agrupador;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("");
+    }
+
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id', 
+            'pais_id' => 'pais_id', 
+            'departamento' => 'departamento', 
+            'observacion' => 'observacion', 
+            'estado' => 'estado', 
+            'baja_logica' => 'baja_logica', 
+            'agrupador' => 'agrupador'
+        );
+    }
 
 }
